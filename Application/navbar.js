8ea47d70b91e1fe459d52d51ff040ab70cedef6a
Mursal Ashraf <ashrafmursal@gmail.com>
2021-09-11 16:57:19 +1000

    added comments to navbar.js

--- Application/navbar.js
+++ Application/navbar.js
@@ -2,7 +2,9 @@
 This may be a way can implement the common values through local storage.
 Have only done it on navbar.html to test it out, if this approach is fine then we
 can move it on to index.html and rest of the pages. Maybe we can set other values
-through this method as well
+through this method as well.
+
+NOT WORKING AS INTENDED - Just testing.
 
 Also: maybe clear local storage if user clicks home ??
 
