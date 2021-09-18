92d85546244399d6f9f8a5a01de92f537513c1c8
Max <maxramm@y7mail.com>
2021-09-18 18:01:28 +1000

    clean up index.js and adjust for topScorers.js to function

--- Application/JS Files/index.js
+++ Application/JS Files/index.js
@@ -1,9 +1,12 @@
 // league IDs and corresponding names
 const footballLeagueId = [39, 140, 61];
-const footballLeague = ['English Premier League', 'La Liga', 'Ligue One'];
+const footballLeague = ['Premier League', 'La Liga', 'Ligue One'];
 
 // storage keys
 const TOP_SCORERS_FOOTBALL_KEY = "topScorersFootball";
+const TOP_ASSISTS_FOOTBALL_KEY = "topAssistsFootball";
+const TOP_RED_FOOTBALL_KEY = "topRedFootball";
+const TOP_YELLOW_FOOTBALL_KEY = "topYellowFootball";
 
 // api data
 const YOUR_KEY = "69c1a7a3dbc22ffc02a24af7ce74aca6";
@@ -23,10 +26,25 @@ var requestOptions = {
 Runs when the page loads 
 */
 function main() {
-    topScorers(footballLeagueId, footballLeague, TOP_SCORERS_FOOTBALL_KEY);
+    //top scorers
+    fetchTopX(footballLeagueId, footballLeague, TOP_SCORERS_FOOTBALL_KEY, 'topscorers');
+
+    //top assists
+    fetchTopX(footballLeagueId, footballLeague, TOP_ASSISTS_FOOTBALL_KEY, 'topassists');
+
+    //top reds 
+    fetchTopX(footballLeagueId, footballLeague, TOP_RED_FOOTBALL_KEY, 'topredcards');
+
+    //top yellows 
+    fetchTopX(footballLeagueId, footballLeague, TOP_YELLOW_FOOTBALL_KEY, 'topyellowcards');
+
+
+    //* used for testing
     data = JSON.parse(localStorage.getItem(TOP_SCORERS_FOOTBALL_KEY));
     console.log(data);
-    // TODO: other api calls here
+
+
+    // TODO: other api calls here as required
     
 }
 
@@ -47,19 +65,9 @@ function checkStorage(key){
 //* Top scorers for current season
 //? do we need top scorers for all seasons?
 
-/*
-responsible for updating/creating localStorage data for top scorers of a particular sport from the current season
-*/
-function topScorers(leagueId, leagueNames, storageKey){
-    if (!checkStorage(storageKey)){
-        fetchTopScorers(leagueId, leagueNames, storageKey);
-    }
-    // TODO implement date checker here (IE if 1 week out of date, then do another call)(low priority)
-}
-
 
 /*
-Function fetches the top scorer data from the league
+Function fetches the top scorer data from the league, but first checks if it already exists in storage
 Inputs :
     - leagueId : a list of integers of the league id's from API-sports
     - leagueNames : a list of strings with the corresponding league names
@@ -67,22 +75,25 @@ Inputs :
 Outputs :
     None
 */
-function fetchTopScorers(leagueId, leagueNames, storageKey){
-    let topScorers = {};
+function fetchTopX(leagueId, leagueNames, storageKey, topXQuery){
+    let topX = {};
+    // if storage is already filled, then don't run api request
+    if (checkStorage(storageKey)) return;
+    // TODO implement date checker here (IE if 1 week out of date, then do another call)(low priority)
 
-    // api call, storing the data in topScorers object
+    // TODO: adjust following code to get data from multiple seasons. Currently only 2021 season
+    // api call, storing the data in topX object
     for(let i = 0; i<leagueId.length; i++) {
-        fetch(`https://v3.football.api-sports.io/players/topscorers?season=2021&league=${leagueId[i]}`, requestOptions)
+        fetch(`https://v3.football.api-sports.io/players/${topXQuery}?season=2021&league=${leagueId[i]}`, requestOptions)
         .then(response => response.json())
         .then((data) => {
-            topScorers[ leagueNames[i] ] = data.response;
+            topX[ leagueNames[i] ] = data.response;
             // put into localStorage
-            localStorage.setItem(storageKey, JSON.stringify(topScorers));  
+            localStorage.setItem(storageKey, JSON.stringify(topX));  
         })
         .catch(error => console.log("error", error));
     }
-    
-   
+
     
 }
 
@@ -100,5 +111,6 @@ Can then use the fixture IDs to get individual match statistics with the fixture
 
 Possible to potentially store all of the fixture ids for the past few seasons in localStorage to make statistic calling easier?
  */
+
 
 main();
