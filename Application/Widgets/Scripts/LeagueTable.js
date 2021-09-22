41e015b0497a1da0c40881f001994b4512ff00d0
Eric <eche0034@student.edu>
2021-09-23 03:16:13 +1000

    some fixes with table output according to league

--- Application/Widgets/Scripts/LeagueTable.js
+++ Application/Widgets/Scripts/LeagueTable.js
@@ -1,13 +1,26 @@
 const footballLeagueId = [39, 140, 61];
 const footballLeague = ['Premier League', 'La Liga', 'Ligue One'];
+const standingTeamIds = [33, 452, 96];
 const league = localStorage.getItem('league')
 
 const STANDINGS_FOOTBALL_KEY = "standingsFootball";
 const FIXTURES_FOOTBALL_TEAM_KEY = "fixturesFootballTeams"
+const TEAM_ID_KEYS = "teamIdsFootball"
 
 const KEY = 'd478817a0fmsh584e04929c59a24p15d9b7jsn193d317fead8';
 
+const requestStandingsFromTeamURL = "https://api-football-v1.p.rapidapi.com/v3/standings?"
+
 const RANKING_NUMS = 20;
+
+const standingsElements = {
+    leagueTableStats: document.getElementById('leagueTableStats'),
+    seasonStandings: document.getElementById('seasonStandings')
+}
+const standingsTableData = {
+    leagueID: footballLeagueId[footballLeague.indexOf(league)],
+    selectedSeason: '2021',
+}
 
 myHeaders = {
     "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
@@ -19,55 +32,97 @@ var requestOptions = {
     headers: myHeaders,
 };
 
+document.getElementById('currentLeague').innerHTML = `Current League: <br> ${league}`;
+
 function checkStorage(key){
     let data = JSON.parse(localStorage.getItem(key));
     return data && data !== null;
 }
 
-function fetchStandings(leagueId, leagueNames, leagueTableStorageKey){
-    let standings = {};
-    // if storage is already filled, then don't run api request
-    if (checkStorage(leagueTableStorageKey)) return;
-
-    for(let i = 0; i < leagueId.length; i++) {
-        fetch(`https://api-football-v1.p.rapidapi.com/v3/standings?season=2020&league=${leagueId[i]}`, requestOptions)
-        .then(response => response.json())
-        .then((data) => {
-            standings[leagueNames[i]] = data.response;
-            // put into localStorage
-            localStorage.setItem(leagueTableStorageKey, JSON.stringify(standings)); 
-        })
-        .catch(error => console.log("error", error));
-    }
+function fetchStandings(){
+    fetchFromAPI(requestStandingsFromTeamURL.concat(`league=${standingsTableData.leagueID}&season=${standingsTableData.selectedSeason}`))
 }
 
-function fetchFixtureByTeamID(leagueId, leagueNames, standingTeamId, leagueTableStorageKey){
-    let standings = {};
-    // if storage is already filled, then don't run api request
-    if (checkStorage(leagueTableStorageKey)) return;
-
-    for(let i = 0; i < leagueId.length; i++) {
-        fetch(`https://api-football-v1.p.rapidapi.com/v3/fixtures?season=2020&league=${leagueId[i]}&team=${standingTeamId}`, requestOptions)
-        .then(response => response.json())
-        .then((data) => {
-            standings[leagueNames[i]] = data.response;
-            // put into localStorage
-            localStorage.setItem(leagueTableStorageKey, JSON.stringify(standings)); 
-        })
-        .catch(error => console.log("error", error));
-    }
+function fetchFromAPI(fetchURL){
+    // api call, storing the data in topScorers object
+    fetch(fetchURL, requestOptions)
+    .then(response => response.json())
+    .then((data) => {
+        localStorage.setItem(STANDINGS_FOOTBALL_KEY, JSON.stringify(data.response))
+        standingsData = data.response
+    })
+    .catch(err => {
+        console.error(err);
+    });
 }
- 
 
-const tableElements = {
-    leagueTableStats: document.getElementById('leagueTableStats')
-}
+// function fetchStandings(leagueId, standingsYear, leagueNames, leagueTableStorageKey){
+//     let standings = {};
+// 
+//     // if storage is already filled, then don't run api request
+//     if (checkStorage(leagueTableStorageKey)) return;
+// 
+//     for(let i = 0; i < leagueId.length; i++) {
+//         fetch(`https://api-football-v1.p.rapidapi.com/v3/standings?season=${standingsYear}&league=${leagueId[i]}`, requestOptions)
+//         .then(response => response.json())
+//         .then((data) => {
+//             standings[leagueNames[i]] = data.response;
+//             // put into localStorage
+//             localStorage.setItem(leagueTableStorageKey, JSON.stringify(standings)); 
+//         })
+//         .catch(error => console.log("error", error));
+//     }
+// 
+// }
+// 
+// function fetchFixtureByTeamID(leagueId, leagueNames, leagueTableStorageKey){
+//     let fixture = {};
+//     // if storage is already filled, then don't run api request
+//     if (checkStorage(leagueTableStorageKey)) return;
+// 
+//     for(let i = 0; i < leagueId.length; i++) {
+//         for(let j = 0; j < teamsLength; j++) {
+//             fetch(`https://api-football-v1.p.rapidapi.com/v3/fixtures?season=2020&league=${leagueId[i]}`, requestOptions)
+//             .then(response => response.json())
+//             .then((data) => {
+//                 fixture[leagueNames[i]] = data.response;
+//                 // put into localStorage
+//                 localStorage.setItem(leagueTableStorageKey, JSON.stringify(fixture)); 
+//             })
+//             .catch(error => console.log("error", error));
+//         }
+//     }
+// }
+
+// function checkId(leagueId, leagueNames){
+//     const teamsId = []
+//     let teamsData;
+//     let tempData;
+// 
+//     tempData = JSON.parse(localStorage.getItem(STANDINGS_FOOTBALL_KEY));
+//     teamsData = tempData[league]
+//     teamsLength = teamsData[0]['league']['standings'][0].length
+// 
+//     for(let i = 0; i < leagueId.length; i++) {
+//         for(let j = 0; j < teamsLength; j++) {
+//             teamsId[leagueNames[i]] = teamsId[leagueNames[i]].concat(teamsData[0]['league']['standings'][j]['team']['id'])
+// 
+//             localStorage.setItem(TEAM_ID_KEYS, JSON.stringify(teamsId))
+//         }
+//     }
+// }
+
+//function updateSeasons(selectSeason){
+//    if (selectSeason == 'seasonStandings'){
+//        standingsTableData.selectedSeason = standingsElements.seasonStandings.options[standingsElements.seasonStandings.selectedIndex].value
+//        fetchFromAPI(requestStandingsFromTeamURL.concat(`league=${standingsTableData.leagueID}&season=${standingsTableData.selectedSeason}`))
+//    }
+//}
 
 function updateStandingsTable() {
 
     let tableData;
-    let tempData;
-
+    
     leagueTableStats.innerHTML = 
         `<tr> 
         <th>Position</th>
@@ -79,21 +134,13 @@ function updateStandingsTable() {
         <th>Points</th>
         <th>History</th>
         <tr>`;
-    
+
     // Loop to access all rows 
-    tempData = JSON.parse(localStorage.getItem(STANDINGS_FOOTBALL_KEY));
-    tableData = tempData[league];
-
-    tempDataFixture = JSON.parse(localStorage.getItem(FIXTURES_FOOTBALL_TEAM_KEY));
-    hiddenTableData = tempDataFixture[league];
-
-
-
+    tableData = JSON.parse(localStorage.getItem(STANDINGS_FOOTBALL_KEY))    
+    // tempDataFixture = JSON.parse(localStorage.getItem(FIXTURES_FOOTBALL_TEAM_KEY));
+    // hiddenTableData = tempDataFixture[league];
     // building table 
     for(var i = 0; i < RANKING_NUMS; i++) {
-        // Get teamID
-        // standingTeamId = [0]['league']['standings'][0][i]['team']['id']
-
         leagueTableStats.innerHTML += 
         `<tr onclick="showHideRow('hidden_row${i}');" class="visible_row">
         <td>${tableData[0]['league']['standings'][0][i]['rank']}</td>
@@ -103,24 +150,32 @@ function updateStandingsTable() {
         <td>${tableData[0]['league']['standings'][0][i]['all']['draw']}</td>
         <td>${tableData[0]['league']['standings'][0][i]['all']['lose']}</td>
         <td>${tableData[0]['league']['standings'][0][i]['points']}</td>
-        <td>${tableData[0]['league']['standings'][0][i]['form']}</td>
-
-        <tr id="hidden_row${i}" class="hidden_row">
-            <td colspan=8>
-                ${hiddenTableData[i]['teams']['home']['name']} VS ${hiddenTableData[i]['teams']['away']['name']}
-            </td>
-        </tr>`;
+        <td>${tableData[0]['league']['standings'][0][i]['form']}</td>`;
+        //for(var j = 0; j < 5; j++) {
+        //    getTeamId();
+        //    leagueTableStats.innerHTML += 
+        //    `<tr id="hidden_row${j}" class="hidden_row">
+        //        <td colspan=8>
+        //            ${hiddenTableData[0]['teams']['home']['name']} VS ${hiddenTableData[0]['teams']['away']['name']}
+        //        </td>
+        //    </tr>`;
+        //}
     }
 }
 
 /*
 Runs when the page loads 
 */
+
+fetchStandings()
+
 function main() {
 
-    fetchStandings(footballLeagueId, footballLeague, STANDINGS_FOOTBALL_KEY)
+    //fetchStandings(footballLeagueId, 2021, footballLeague, STANDINGS_FOOTBALL_KEY)
 
-    fetchFixtureByTeamID(footballLeagueId, footballLeague, 33, FIXTURES_FOOTBALL_TEAM_KEY)
+    //checkId(footballLeagueId, footballLeague)
+
+    //fetchFixtureByTeamID(footballLeagueId, footballLeague, FIXTURES_FOOTBALL_TEAM_KEY)
 
     updateStandingsTable();
 }
