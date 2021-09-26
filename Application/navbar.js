af9943988e91e2c58a8b828e08acde108a921224
Mursal Ashraf <ashrafmursal@gmail.com>
2021-09-26 14:55:29 +1000

    added logos on the options page

--- Application/navbar.js
+++ Application/navbar.js
@@ -15,3 +15,40 @@ Mursal
 function setLeagueStorage(value){
     window.localStorage.setItem('league', value)
 }
+
+//set logo based of local storage value in options page
+const leageVal = window.localStorage.getItem('league');
+
+function getImagePath(local_league){
+    var ret_val = "";
+    if (local_league == "Premier League"){
+        ret_val = "./images/soccer/premier-league.png";
+    } else if (local_league == "La Liga"){
+        ret_val = "./images/soccer/la-liga.png";
+    } else if (local_league == "Ligue One") {
+        ret_val = "./images/soccer/ligue-one.png";
+    }
+
+    return ret_val;
+}
+
+function setImageSrc(path){
+    var image_left = document.getElementById('league_logo_left');
+    var image_right = document.getElementById('league_logo_right');
+
+    //set paths and dimensions
+    image_left.src = path;
+    image_left.style.height = '50px';
+    image_left.style.width = '50px';
+
+    image_right.src = path;
+    image_right.style.height = '50px';
+    image_right.style.width = '50px';
+
+    //set alt attributes
+    image_left.alt = 'logo of ' + leageVal
+    image_right.alt = 'logo of ' + leageVal
+}
+
+var imagePath = getImagePath(leageVal)
+setImageSrc(imagePath);
